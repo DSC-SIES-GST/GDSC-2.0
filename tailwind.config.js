d92ee5b80b679c954f59e0de3b763a4603838f23
Shivam Gupta <shivamrvgupta@gmail.com>
2023-01-06 00:45:14 +0530

    staged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        linear_org: 'hsl(0,100,67,100%)',
+        linear_prl: 'hsl(281,94,70,100%)',
+        linear_ylw: 'hsl(44,100,81,100%)',
+        linear_yel: 'hsl(40,100,66,100%)',
+        black: '#000',        
+        white: '#fff'      
+      }
+    },
   },
   plugins: [],
 }
